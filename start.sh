32522042114cad71e7f31224b0043f2991cb3a44
mldreiling14 <mldreil@unc.edu>
2026-01-25 15:25:07 -0500

    Working start.sh script

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ docker run -d \
 	--name bios992 \
 	-p 8787:8787 \
 	-e PASSWORD=password \
-	-v "$HOME/bios992/:/home/rstudio/project" \
+	-v "/c/Users/drdre/bios992/:/home/rstudio/project" \
 	bios992
